1391e4a4f015433201dbe8421998718a79cfdadc
Gautier <gautier.fournet@gmail.com>
2023-10-21 17:53:36 +0200

    Code source

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Burhan from './burhan.jpg'
+
+function App() {
+  return (
+    <div className="App">
+      <img src={Burhan} alt=''/>
+      <h1>J'aime la vie</h1>
+    </div>
+  );
+}
+
+export default App;
