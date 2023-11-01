557ef993d410cbd861e9d4a49ecf9389b8a36029
Gautier <gautier.fournet@gmail.com>
2023-11-01 16:14:27 +0100

    Supprimer Link

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import App from './App.jsx';
 import MesProjets from "./pages/MesProjets.jsx";
 import NoPage from "./pages/NoPage.jsx";
